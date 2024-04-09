f77c5cf3fc50788e91ab4ffa73202b7888c69155
obadahalak <69998512+obadahalak@users.noreply.github.com>
2024-04-09 16:49:16 +0300

    update-

--- app/Traits/UploadImage.php
+++ app/Traits/UploadImage.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Storage;
 
 trait UploadImage
 {
-    public function upload($image, $folderName)
-    {
+    public function upload($image, $folderName){
         return $image->store($folderName, 'public');
     }
 
-    public function deleteImage($image, $folderName)
-    {
-        if (Storage::exists('public/' . $image, $folderName)) {
-            Storage::delete('public/' . $image, $folderName);
-        }
+    public function deleteImage($image, $folderName){
+        
+        if (Storage::exists('public/' . $image, $folderName)) 
+           return Storage::delete('public/' . $image, $folderName);
+        
     }
 
-    public function updateImage($image, $folderName)
-    {
-        $this->deleteImage($image, $folderName);
+    public function updateImage($image, $folderName){
+        if($this->deleteImage($image,$folderName))
+          return $this->upload($image,$folderName);
+        
     }
 }
